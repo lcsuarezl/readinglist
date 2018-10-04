80d9bab9bdd4e6b6344650539c04fc14b888d1d3
Leon.Suarez <leon.suarez@Admins-MacBook-Pro.local>
2018-10-04 15:21:41 -0500

    Start sample project spring maning reading list

---
+++ src/main/java/com/maning/readinglist/ReadingListDemoApplication.java
@@ -0,0 +1,12 @@
+package com.maning.readinglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingListDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingListDemoApplication.class, args);
+	}
+}
